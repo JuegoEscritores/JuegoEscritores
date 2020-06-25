9e8a22fd8fb15794e2d228a9b8d61702790d9911
JuegoEscritores <juegoEscritores@gmail.com>
2020-06-25 13:33:00 -0500

    Updates

---
+++ precache-manifest.a5a8b63925fda49eadd79b6db8589aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eb68e78dfe0d4eb79effebcd15bf11",
+    "url": "/JuegoEscritores/index.html"
+  },
+  {
+    "revision": "3c8b00097afbf484b83f",
+    "url": "/JuegoEscritores/static/css/main.f4ed7bc0.chunk.css"
+  },
+  {
+    "revision": "d05d8df7d3340c7282e1",
+    "url": "/JuegoEscritores/static/js/2.1ffcf48c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JuegoEscritores/static/js/2.1ffcf48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8b00097afbf484b83f",
+    "url": "/JuegoEscritores/static/js/main.000477c4.chunk.js"
+  },
+  {
+    "revision": "fcd54b2ada66e891c3a9",
+    "url": "/JuegoEscritores/static/js/runtime-main.6c58ffba.js"
+  }
+]);
--- src/models/infoEmail.ts
+++
@@ -1,7 +0,0 @@
-
-export const InfoEmail ={
-    userName : "",
-    level : 1,
-    scoreLevel: 0,
-    author: ""
-}
